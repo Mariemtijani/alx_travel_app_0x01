393d2150b3e95c522d5c48e1f0be9b34c5e1658b
Mariem Tijani <mariemtijani60@gmail.com>
2025-06-15 12:35:24 +0100

    s

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -14,7 +14,9 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('listings.urls')),
 ]
